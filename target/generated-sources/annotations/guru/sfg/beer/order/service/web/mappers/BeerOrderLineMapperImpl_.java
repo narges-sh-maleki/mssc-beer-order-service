449ba29af2f1850f0d4f8dfa27e7ef26719aa065
narges_sh_maleki <narges.sh.maleki@gmail.com>
2020-11-13 11:39:07 +0100

    configured resttemplate logging

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-10T13:45:01+0100",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (GraalVM Community)"
+    date = "2020-11-13T10:40:11+0100",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
 @Qualifier("delegate")
