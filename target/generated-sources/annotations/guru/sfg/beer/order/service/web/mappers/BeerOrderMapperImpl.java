7d7eb2dfcb7fb0574a834e149f545589fd39c014
narges_sh_maleki <narges.sh.maleki@gmail.com>
2020-11-14 15:00:46 +0100

    connected to mysql and added localmysql  profile

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-13T10:40:11+0100",
+    date = "2020-11-14T10:31:53+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
